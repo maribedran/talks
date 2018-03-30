1d9cbe83f7afe5e670d9c93a46ffce66aba70363
Mariana Bedran Lesche <maribedran@gmail.com>
2018-03-30 20:42:43 -0300

    Adiciona configurações de log do Django

--- grupy_sulflu/project/project/settings.py
+++ grupy_sulflu/project/project/settings.py
@@ -119,3 +119,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+        'class': 'logging.StreamHandler',
+    },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
+
+
